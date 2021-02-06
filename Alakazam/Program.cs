5c71ef20d2d5a1c4c37785337dfe2f5638541fb1
Laura Bilíkova <bilikovalaura@bilikova.onmicrosoft.com>
2021-02-06 18:43:22 +0100

    Yay

---
+++ Alakazam/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Alakazam
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			int a = 0;
+			string b = "alaka";
+			string c = "zam";
+			while (a != 100)
+			{
+				a = ++a;
+				Console.WriteLine(a);
+				if (a % 3 == 0 && a % 4 == 0)
+				{
+					Console.WriteLine(b + c);
+				}
+				else if (a % 3 == 0)
+				{
+					Console.WriteLine(b);
+				}
+				else if (a % 4 == 0)
+				{
+					Console.WriteLine(c);
+				}
+				
+			}
+		}
+    }
+}
